46255ece00ecf1c52300b017d3278056fda2795c
Sanchit <50187427+sskeni@users.noreply.github.com>
2025-08-25 16:14:09 -0700

    Added more stats and reset button to end screen

--- Assets/Scripts/UI/EndScreenMenu.cs
+++ Assets/Scripts/UI/EndScreenMenu.cs
@@ -13,8 +13,11 @@ public class EndScreenMenu : MonoBehaviour
     // UI References
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI levelText;
-    public TextMeshProUGUI attackText;
     public TextMeshProUGUI healthText;
+    public TextMeshProUGUI healthRegenRateText;
+    public TextMeshProUGUI attackText;
+    public TextMeshProUGUI critRateText;
+    public TextMeshProUGUI critDamageText;
     public TextMeshProUGUI roomsClearedText;
     public TextMeshProUGUI enemiesKilledText;
 
@@ -51,12 +54,23 @@ public class EndScreenMenu : MonoBehaviour
         float seconds = TimeSpan.FromSeconds(totalTimeInSeconds).Seconds;
 
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
         levelText.text = "Level " + PlayerController.Instance.playerLevel.level;
-        attackText.text = "Attack: " + PlayerController.Instance.attackDamage;
         healthText.text = "Health: " + PlayerController.Instance.playerHealth.maxHealth;
+        healthRegenRateText.text = "Health Regen Rate: " + PlayerController.Instance.playerHealth.regenRate;
+        attackText.text = "Attack: " + PlayerController.Instance.attackDamage;
+        critRateText.text = "Critical Rate: " + PlayerController.Instance.critChance + "%";
+        critDamageText.text = "Critical Damage: " + PlayerController.Instance.critDamage + "%";
+
         roomsClearedText.text = "Rooms Cleared: " + RoomHandler.Instance.roomsCleared;
         enemiesKilledText.text = "Enemies Killed: " + RoomHandler.Instance.enemiesKilled;
         gameObject.SetActive(true);
+    }
+
+    // Restarts the game
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // Returns to the main menu
@@ -64,7 +78,7 @@ public class EndScreenMenu : MonoBehaviour
     {
         StartCoroutine(LoadSceneAsync());
     }
-
+    
     // Loads the main menu asyncronously
     private IEnumerator LoadSceneAsync()
     {
